68064a0b39ca5ca5720e5d4591d1a80cc88cfa34
ianbgroves <46450745+ianbgroves@users.noreply.github.com>
2020-04-08 17:04:16 +0100

    changes w/ meeting with AGF 04_08_20

--- reaction_diffusion_solver_adding_menshykau_eqs.py
+++ reaction_diffusion_solver_adding_menshykau_eqs.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Apr  8 12:28:05 2020
+
+@author: Ian_b
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Mon Apr  6 10:10:22 2020
 
 @author: Ian_b
@@ -38,27 +45,50 @@ import scipy.sparse.linalg
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Parameter values (AGF's)
+# a = 0.2
+# b = 2.0
+# gamma = 0.2
+# d_s = 1
+# d_f = 10
+# L = 1
 
-#parameters in a simple model of cross repression
-##update documentation to report what eqs were solving.
-rhoS = 1
-rhoF = 2
-n = 1
-degs = 1
-degf = 1
-d_s = 1
+
+#menshykau et al. plos comp biol params
+
+a = 0.2
+b = 2.0
+gamma = 0.2
+d_s = 5
 d_f = 1
 L = 1
 
 
-
+rhoS = 1600
+rhoF = 3.5
+rhoP = 0.6
+n = 1
+ds = 0.2
+df = 5
+dc = 1.6
+dp = 1
+v = 5
 # Reaction terms
-def reaction_term_s(s, f):
-    return rhoS/(f**n+1) - degs*s
+# def reaction_term_s(s, f):
+#     return rhoS * (f**n/f**n+1) - ds*s
 
-def reaction_term_f(s, f):
-    return rhoF/(s**n + 1) - degf*f
+# def reaction_term_f(s, f):
+#     return rhoF - df*f
     
+
+def reaction_term_s(s, f, p):
+    return rhoS * (f**n/f**n+1) - ds*s
+
+def reaction_term_f(s, f, p):
+    return rhoF - df*f
+    
+def reaction_term_p(s, f, p):
+    return rhoP - dp*p + (v-2*dc)*p**2*s
 def pde_solver_with_no_flux_bcs(Nx, T):
     x = np.linspace(0, L, Nx+1)   # mesh points in space
     dx = x[1] - x[0]
@@ -67,12 +97,13 @@ def pde_solver_with_no_flux_bcs(Nx, T):
     Nt = int(round(T/float(dt)))
     t = np.linspace(0, T, Nt+1)   # mesh points in time
 
-    s = np.zeros(Nx+1)            # solution array for u at t[n+1]
-    f = np.zeros(Nx+1)            # solution array for v at t[n+1]
+    s = np.zeros(Nx+1)            # solution array for s at t[n+1]
+    f = np.zeros(Nx+1)            # solution array for f at t[n+1]
+    p = np.zeros(Nx+1)            # solution array for p at t[n+1]
     
-    s_old = np.zeros(Nx+1)        # solution array for u at t[n]
-    f_old = np.zeros(Nx+1)        # solution array for v at t[n]
-
+    s_old = np.zeros(Nx+1)        # solution array for s at t[n]
+    f_old = np.zeros(Nx+1)        # solution array for f at t[n]
+    p_old = np.zeros(Nx+1)        # solution array for p at t[n]
     # Precompute sparse matrices (scipy format)
     diagonal = (1 + d_s*dt/(dx**2))*np.ones(Nx+1)
     upper = -0.5*(d_s*dt/(dx**2))*np.ones(Nx)
@@ -105,15 +136,18 @@ def pde_solver_with_no_flux_bcs(Nx, T):
     f_matrix_rhs = scipy.sparse.diags(diagonals=[diagonal, lower, upper],
                            offsets=[0, -1, 1], shape=(Nx+1, Nx+1),
                            format='csr')
-        
+    
+
+
+    
     # Representation of right-hand sides
     rhs_s = np.zeros(Nx+1)
     rhs_f = np.zeros(Nx+1)
-    
+    rhs_p = np.zeros(Nx+1)
     # Set initial condition
-    s_old = 1+np.tanh((0.25-x)*10) 
-    f_old = 1+np.tanh((0.25-x)*10) 
-
+    s_old = (a + b)*np.ones(Nx+1) + 0.01*np.random.rand(Nx+1)
+    f_old = (b/((a+b)**2))*np.zeros(Nx+1) + 0.01*np.random.rand(Nx+1)
+    p_old = #####???
     # Time loop
     for n in range(0, Nt):
         rhs_s = s_matrix_rhs*s_old + dt*reaction_term_s(s_old, f_old)
@@ -128,7 +162,7 @@ def pde_solver_with_no_flux_bcs(Nx, T):
     return s, f, x, t
 
 
-s, f, x, t = pde_solver_with_no_flux_bcs(Nx=50, T=10.0)
+s, f, x, t = pde_solver_with_no_flux_bcs(Nx=50, T=1.0)
 
 
 # Call the PDE solver and plot u(x,1.0) and v(x,1.0)
